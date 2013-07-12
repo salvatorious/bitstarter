c060d0a61520ad384646b74488662dc9e2742228
Iconoblast <salvatore.the.candela@gmail.com>
2013-07-12 02:10:41 +0000

    Take 2.

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var buffer = new Buffer(fs.readFileSync('index.html'), 'utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.ToString());
 });
 
 var port = process.env.PORT || 5000;
